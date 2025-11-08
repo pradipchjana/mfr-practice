cb739a8d5c305d7f5435e5070896ffacb0ebc3e5
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-11-08 18:07:59 +0000

    code for first problem

---
+++ 01_festival_ribvbon_count.js
@@ -0,0 +1,5 @@
+const countBlueRibon = (array) => {
+    return array.filter(function (value) { return value === 'blue'}).reduce( function (count) { return count + 1}, 0);
+}
+
+console.log(countBlueRibon(["red", "blue", "red", "green", "red", "blue"]))
