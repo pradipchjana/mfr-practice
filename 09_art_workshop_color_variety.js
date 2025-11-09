b0f1c84a4768e2080c3614e3d72581984bc2334d
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-11-09 10:19:48 +0000

    problem 9

---
+++ 09_art_workshop_color_variety.js
@@ -0,0 +1,18 @@
+// ### **9. Art Workshop Color Variety**
+
+// Paint colors used in sessions:
+
+// ```
+// ["blue", "yellow"]
+// ["yellow", "green"]
+// ["blue"]
+// ```
+
+// Find unique colors used.
+
+const findUniqueColor = (inputArray) => {
+    const flatArray = inputArray.flat();
+    return flatArray.filter((color, index) => flatArray.indexOf(color) === index);
+}
+
+console.log(findUniqueColor([["blue", "yellow"], ["yellow", "green"], ["blue"]]));
