c0938ff0ad06c3bf96fb2dc17adec7d68e86e53b
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-11-09 09:58:40 +0000

    code for problem 7

---
+++ 07_weather_sensor_validation.js
@@ -0,0 +1,20 @@
+// ### **7. Weather Sensor Validation**
+
+// Several temperature sheets:
+
+// ```
+// [22, 23]
+// [25, 24, 22]
+// [29]
+// ```
+
+// Check if **every** recorded temperature is below 32.
+
+const hasAllBellow32Temperature = (inputArray) => {
+    const flatArray = inputArray.flat();
+    return flatArray.every((temperature) => temperature < 32);
+}
+
+console.log(hasAllBellow32Temperature([[22, 23, 12], [24], [29, 22]]));
+console.log(hasAllBellow32Temperature([[12], [23, 32]]));
+
