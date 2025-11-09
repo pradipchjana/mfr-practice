e6bbdf5331b8c1347cab8305eeb950537508b1bf
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-11-09 09:01:10 +0000

    code for 3rd code

---
+++ 03_birdwatching_duplicate_removal.js
@@ -0,0 +1,15 @@
+// ### **3. Birdwatching Duplicate Removal**
+
+// A birdwatcher notes species seen during a morning walk:
+
+// ```
+// ["sparrow", "crow", "sparrow", "eagle", "crow"]
+// ```
+
+// Create a list of the species without repeats, preserving the order first seen.
+
+const removeDuplicate = (array) => {
+    return array.filter((element, index) => array.indexOf(element) === index);
+}
+
+console.log(removeDuplicate(["sparrow", "crow", "sparrow", "eagle", "crow"]))
