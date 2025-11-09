c952a96886f2e8cb907f5b975d059072e21b1d08
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-11-09 09:19:30 +0000

    code for No. 4 problem

---
+++ 04_classroom_attendence_check.js
@@ -0,0 +1,18 @@
+// ### **4. Classroom Attendance Check**
+
+// A class records names of students present for each period:
+
+// ```
+// ["Asha", "Ravi", "Neel"]
+// ["Ravi"]
+// ["Asha", "Meera"]
+// ```
+
+// Determine which distinct students attended at least once.
+
+const findAttendedStudent = (inputArray) => {
+    const flatArray = inputArray.flat();
+    return flatArray.flat().filter((element, index) => flatArray.indexOf(element) === index);
+}
+
+console.log(findAttendedStudent([["pradip", "rani", "sayan"], ['pradip', 'rakesh'], ["rani", 'shivaji']]));
