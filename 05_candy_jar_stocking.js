dbcf4c5cd26afa3e575db356205d17be73998fdb
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-11-09 09:26:23 +0000

    problem 5

---
+++ 05_candy_jar_stocking.js
@@ -0,0 +1,20 @@
+// ### **5. Candy Jar Stocking**
+
+// A store logs candy refills like this:
+
+// ```
+// [5, 3]
+// [2]
+// [4, 1]
+// ```
+
+// Find the total number of candies added.
+
+const countCandies = (inputArray) => {
+    const flatArray = inputArray.flat();
+    return flatArray.reduce((value, result) => {return result + value; }, 0);
+}
+
+console.log(countCandies([[4], [5, 6, 2, 1], [3, 2]]));
+console.log(countCandies([[0], [1, 3, 4] ,[9], [3, 4]]));
+
