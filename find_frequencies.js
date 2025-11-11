810d1e467950ce355c86beeefcb15ffd5d4348f2
Pradip Jana <janaprdip501@gmail.com>
2025-11-11 11:51:33 +0530

    find frequency using object

--- find_frequencies.js
+++ find_frequencies.js
@@ -1,15 +1,25 @@
 const data = ["a", "b", "c", "c", "d", "b", "b", "a", "b", "c", "b"];
 
-const countOccurrences = (occurrences, element) => {
-  const frequecyArray = occurrences.find((x) => x[0] === element);
-  if (frequecyArray !== undefined) {
-    frequecyArray[1] += 1;
+const countOccurrences = (occurrences, target) => {
+  const element = occurrences.find((x) => x[0] === target);
+  if (element) {
+    element[1] += 1;
     return occurrences;
   }
-  occurrences.push([element, 1]);
+  occurrences.push([target, 1]);
+  return occurrences;
+};
+
+const frequencieTable = (occurrences, target) => {
+  if (!(target in occurrences)) {
+    occurrences[target] = 0;
+  }
+  occurrences[target]++;
   return occurrences;
 };
 
 const findFrequencies = (array) => array.reduce(countOccurrences, []);
+const findFrequencieTable = (array) => array.reduce(frequencieTable, {});
 
 console.log(findFrequencies(data));
+console.log(findFrequencieTable(data));
